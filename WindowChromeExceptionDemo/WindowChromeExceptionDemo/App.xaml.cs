e8c3ecbf25de68905149ca07cea5f8b64f992e08
Venkat-Muthu <Venkat.Muthu@recordsure.com>
2016-11-28 23:26:42 +0000

    WindowChrome  exception demo
    WindowChrome "Parameter count mismatch." exception demo

---
+++ WindowChromeExceptionDemo/WindowChromeExceptionDemo/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WindowChromeExceptionDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+            Current.DispatcherUnhandledException += CurrentOnDispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+        }
+
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs unobservedTaskExceptionEventArgs)
+        {
+            MessageBox.Show(unobservedTaskExceptionEventArgs.Exception.Message);
+        }
+
+        private void CurrentOnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs dispatcherUnhandledExceptionEventArgs)
+        {
+            MessageBox.Show(dispatcherUnhandledExceptionEventArgs.Exception.Message);
+        }
+
+        private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            MessageBox.Show(((Exception) unhandledExceptionEventArgs.ExceptionObject).Message);
+        }
+    }
+}
---
+++ WindowChromeExceptionDemo/WindowChromeExceptionDemo/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Microsoft.Windows.Shell;
+
+namespace WindowChromeExceptionDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var windowChrome = new WindowChrome {CaptionHeight = 2};
+            windowChrome.CaptionHeight = 1;
+        }
+    }
+}
